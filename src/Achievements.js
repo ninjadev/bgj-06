9d6a70485aa1553947fe56b9c00d69ae5ba3d1c6
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-10-26 07:24:25 +0200

    Add simple achievements framework

---
+++ src/Achievements.js
@@ -0,0 +1,26 @@
+function Achievements(){
+    var that = this;
+    $(function(){
+        that.template = $('.achievement.template');
+    });
+    this.achievements = {
+        first: {
+            name: 'First click',
+            description: 'Clicked for the first time!'
+        }
+    };
+}
+
+Achievements.prototype.give = function(achievement_key){
+    var achievement = this.achievements[achievement_key];
+    if(achievement.achieved){
+        return;
+    }
+    achievement.achieved = true;
+    var achievement_div = this.template.clone().removeClass('template');
+    achievement_div.find('.image').attr('src',
+            '/res/achievements/' + achievement_key + '.png');
+    achievement_div.find('.name').text(achievement.name);
+    achievement_div.find('.description').text(achievement.description);
+    Toast(achievement_div[0].outerHTML);
+}
