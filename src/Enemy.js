1ae4baa52c8f6d17402cff902c6dca3c1d9ce9bf
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2013-10-26 22:48:59 +0200

    Add enemy effects, with the initial SpeedEffect
    Adds an array of all the effects on an enemy. onApply, update and render
    gets called :)

--- src/Enemy.js
+++ src/Enemy.js
@@ -2,11 +2,18 @@ function Enemy(x, y, hp, speed, sprites, bounty){
   this.x = x;
   this.y = y;
 
-    this.sprites = sprites;
+  this.sprites = sprites;
 
   this.hp = hp;
-  this.speed = speed;
+  this.baseSpeed = speed;
   this.killRadius = 0.0086*GU;
+
+  this.effects = [];
+
+  //Example of how to add enemy
+  //TODO: Remove me
+  //this.addEffect(new SpeedEffect(0.5));
+
 
   this.width = 40;
   this.height = 40;
@@ -17,23 +24,27 @@ function Enemy(x, y, hp, speed, sprites, bounty){
 
 Enemy.prototype.render = function(ctx){
 
-    var sprite = this.sprites.walking[this.animation_ticker|0];
-    var scaler = sprite.width * GU * 0.000015 * 0.3;
-    ctx.save();
-    ctx.translate(this.x * GU, this.y * GU);
-    if(this.x > CENTER.x){
-        ctx.scale(-scaler, scaler);
-    }else{
-        ctx.scale(scaler, scaler);
-    }
+  this.renderEffects(ctx);
+  var sprite = this.sprites.walking[this.animation_ticker|0];
+  var scaler = sprite.width * GU * 0.000015 * 0.3;
+  ctx.save();
+  ctx.translate(this.x * GU, this.y * GU);
+  if(this.x > CENTER.x){
+    ctx.scale(-scaler, scaler);
+  }else{
+    ctx.scale(scaler, scaler);
+  }
 
-    ctx.rotate((CENTER.y - this.y) / 8);
+  ctx.rotate((CENTER.y - this.y) / 8);
 
-    ctx.drawImage(sprite, -sprite.width / 2, - sprite.height / 2);
-    ctx.restore();
+  ctx.drawImage(sprite, -sprite.width / 2, - sprite.height / 2);
+  ctx.restore();
 }
 
 Enemy.prototype.update = function(){
+  this.speed = this.baseSpeed;
+  this.effectsUpdate();
+
   //Make a vector from enemy to center
   var movex = CENTER.x - this.x;
   var movey = CENTER.y - this.y;
@@ -62,6 +73,12 @@ Enemy.prototype.update = function(){
   }
 
   return true;
+}
+
+Enemy.prototype.renderEffects = function(ctx){
+  for(var i = 0; i < this.effects.length; i++){
+    this.effects[i].render(ctx, this);
+  }
 }
 
 Enemy.spawnRandom = function(hp, speed, sprites, bounty){
@@ -97,4 +114,24 @@ Enemy.prototype.kill = function () {
   var index = sm.states.game.enemies.indexOf(this);
   sm.states.game.enemies.splice(index, 1);
   sm.states.game.cash.add(this.bounty);
+}
+
+Enemy.prototype.effectsUpdate = function(){
+  for(var i = 0; i < this.effects.length; i++){
+    this.effects[i].update(this);
+  }
+}
+
+/** If the enemy has an effect of the same type, it replaces that effect.
+ If not it adds the effect to the enemy. */
+Enemy.prototype.addEffect = function(effect){
+  for(var i = 0; this.effects.length; i++){
+    if(this.effects[i] instanceof effect.constructor){
+      this.effects[i] = effect;
+      return;
+    }
+  }
+
+  this.effects.push(effect);
+  effect.onApply(this);
 }
