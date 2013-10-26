a43aaf64ea0e015503e76388d66ff1a973e54475
benedicte <benediof@gmail.com>
2013-10-26 03:24:50 +0200

    I made an ugly brown box move

--- src/ExampleState.js
+++ src/ExampleState.js
@@ -5,6 +5,8 @@ function ExampleState(){
 ExampleState.prototype.init = function(){
     this.elements = [];
     this.t = 0;
+    this.boxx = 0;
+    this.boxy = 0; 
 }
 
 ExampleState.prototype.pause = function(){
@@ -15,8 +17,30 @@ ExampleState.prototype.resume = function(){
 }
 
 ExampleState.prototype.render = function(ctx){
+	ctx.fillStyle="#993322";
+	ctx.fillRect(this.boxx,this.boxy,150,75);
 }
 
 ExampleState.prototype.update = function(){
     this.t++;
+   	var up = 38;
+   	var down = 40;
+   	var right = 39;
+   	var left = 37;
+
+   	if(KEYS[up]){
+   		this.boxy-=5;
+   	}
+   	
+   	if(KEYS[down]){
+   		this.boxy+=5;
+   	}
+   	if(KEYS[left]){
+   		this.boxx-=5;
+   	}
+   	
+   	if(KEYS[right]){
+   		this.boxx+=5;
+   	}
+
 }
