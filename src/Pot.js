20142f9508696a75c1873bf347087bb87155182a
iver56 <iver56@hotmail.com>
2013-10-26 17:57:53 +0200

    Improved pot affordance and feedback. Also refactored yo in bootstrap.

--- src/Pot.js
+++ src/Pot.js
@@ -1,13 +1,18 @@
+/**
+ * No, we're not talking about marihuana
+ * @constructor
+ */
 function Pot() {
   this.position = {
     x: CENTER.x,
     y: CENTER.y
   };
+  this.clickScaler = 1;
 }
 
 Pot.prototype.render = function() {
   ctx.save();
-  var scaler = this.sprite.width * GU * 0.000015;
+  var scaler = this.sprite.width * GU * 0.000015 * this.clickScaler;
   ctx.translate(this.position.x * GU, this.position.y * GU);
   ctx.scale(scaler, scaler);
   ctx.drawImage(this.sprite, -this.sprite.width / 2, -this.sprite.height / 2);
@@ -15,5 +20,9 @@ Pot.prototype.render = function() {
 }
 
 Pot.prototype.update = function(){
+  this.clickScaler += 0.1 * (1 - this.clickScaler);
+}
 
+Pot.prototype.click = function() {
+  this.clickScaler = 0.8;
 }
