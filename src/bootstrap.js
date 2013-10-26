4bdfebff87acfab913d2bcf54f16c028f8d2a5f3
iver56 <iver56@hotmail.com>
2013-10-26 04:18:42 +0200

    Added Particle

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,7 +2,7 @@ DEBUG = true;
 
 missedGFXFrames = 0;
 
-/* smoothstep interpolaties between a and b, at time t from 0 to 1 */
+/* smoothstep interpolates between a and b, at time t from 0 to 1 */
 function smoothstep(a, b, t) {
   var v = t * t * (3 - 2 * t);
   return b * v + a * (1 - v);
