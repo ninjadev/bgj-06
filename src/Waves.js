ef4b8e2587c7eb9d72c3ed0c741f71713642eb64
Péter H. Gombos <peter@gombos.no>
2013-10-27 14:16:45 +0100

    Enemies scales with the wave number.

--- src/Waves.js
+++ src/Waves.js
@@ -2,43 +2,43 @@ WAVES = [
     {
         monsters: ["dog"]
       , numberOfMonsters: 5
-      , scaling: 1
-      , duration: 10000
+      , scaling: 0
+      , duration: 5000
     }
   , {
         monsters: ["bear"]
       , numberOfMonsters: 10
-      , scaling: 1
-      , duration: 15000
+      , scaling: 0
+      , duration: 10000
     }
   , {
         monsters: ["elephant"]
       , numberOfMonsters: 15
-      , scaling: 1
+      , scaling: 0
       , duration: 15000
     }
   , {
         monsters: ["dog", "bear"]
       , numberOfMonsters: 25
-      , scaling: 1.1
+      , scaling: 0.01
       , duration: 22000
     }
   , {
         monsters: ["bear", "elephant"]
       , numberOfMonsters: 25
-      , scaling: 1.2
+      , scaling: 0.02
       , duration: 20000
     }
   , {
         monsters: ["dog", "elephant"]
       , numberOfMonsters: 25
-      , scaling: 1.2
+      , scaling: 0.02
       , duration: 20000
     }
   , {
         monsters: ["dog", "elephant", "bear"]
       , numberOfMonsters: 30
-      , scaling: 1.3
+      , scaling: 0.02
       , duration: 20000
     }
 ]
