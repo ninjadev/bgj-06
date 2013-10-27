43b05c650fcbdb76822df646c10a663fc3e35df3
Stian Jensen <me@stianj.com>
2013-10-27 08:26:28 +0100

    Queue toasts

--- src/Toast.js
+++ src/Toast.js
@@ -1,11 +1,21 @@
 Toast = (function(){
     var toast_template;
+    var active_toast;
+    var toast_queue = [];
     $(function(){
         toast_template = $('.toast.template');
     });
 
     function Toast(message){
         console.log(toast_template, message);
+        toast_queue.push(message);
+        if (active_toast == undefined) {
+          printMessage(toast_queue.shift());
+        }
+    }
+
+    function printMessage(message) {
+        active_toast = message;
         var toast = toast_template.clone()
             .removeClass('template');
         toast.find('.message').html(message);
@@ -24,6 +34,10 @@ Toast = (function(){
                 duration: 500,
                 queue: false
             }, 'easeinout');
+            active_toast = undefined;
+            if (toast_queue.length > 0) {
+              printMessage(toast_queue.shift());
+            }
             toast.fadeOut(500, function(){
                 toast.remove();
             });
