8bcdd2bc9fd52a9f558bbcf4197dad7e1b76e33e
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-10-27 02:37:09 +0100

    Add Main menu

---
+++ src/MenuState.js
@@ -0,0 +1,84 @@
+function MenuState(){
+}
+
+MenuState.prototype.init = function(){
+  this.bg_img = loadImage('res/bg.png');
+  this.vignette_img = loadImage('res/vignette.png');
+  this.menu_img = loadImage('res/menu.png');
+  this.bg_title_img = loadImage('res/bg-title.png');
+
+  this.key_cooldown = 0;
+
+  this.elements = [];
+
+  this.y_values = [1.8, 3.3, 4.8];
+  this.y_time = 1;
+  this.y = this.y_values[0];
+  this.start_y = this.y_values[0];
+  this.end_y = this.y_values[0];
+  this.selected = 0;
+}
+
+MenuState.prototype.setY = function(y){ 
+  this.start_y = this.y;
+  this.end_y = y;
+  this.y_time = 0;
+}
+
+MenuState.prototype.select = function(selected){
+    console.log(selected);
+  this.selected = selected;
+  this.setY(this.y_values[this.selected]);
+}
+
+MenuState.prototype.pause = function(){
+}
+
+MenuState.prototype.resume = function(){
+}
+
+MenuState.prototype.render = function(ctx){
+  ctx.save();
+  var scaler = 16*GU/this.bg_img.width;
+  ctx.scale(scaler, scaler);
+  ctx.drawImage(this.bg_img, 0, 0);
+  ctx.drawImage(this.vignette_img, 0, 0);
+  ctx.drawImage(this.bg_title_img, 0, 0);
+  ctx.restore();
+
+  ctx.fillStyle = '#8742d1';
+  ctx.fillRect(0, this.y*GU, 16*GU, 0.93*GU);
+
+  ctx.save();
+  var scaler = 16*GU/this.bg_img.width;
+  ctx.scale(scaler, scaler);
+  ctx.drawImage(this.menu_img, 0, 0);
+  ctx.restore();
+}
+
+MenuState.prototype.update = function(){
+  this.key_cooldown && this.key_cooldown--;
+  this.y_time += 0.032;
+  if(this.y_time < 1){
+    this.y = smoothstep(this.start_y, this.end_y, this.y_time);
+  }else if (this.y_time > 1){
+    this.y_time = 1;
+    this.y = this.end_y;
+  }
+
+
+  if(!this.key_cooldown){
+    if(KEYS[40]){ /* key down */
+      this.selected + 1 < this.y_values.length && this.select(this.selected + 1);
+      this.key_cooldown = 10;
+    }
+    if(KEYS[38]){ /* key up */
+      this.selected - 1 >= 0 && this.select(this.selected - 1);
+      this.key_cooldown = 10;
+    }
+    if(KEYS[13]){ /* key enter */
+      this.key_cooldown = 10;
+      sm.changeState('game');
+    }
+  }
+}
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -108,12 +108,13 @@ function bootstrap(){
   /* add game states here */
 
   sm.addState("game", new GameState());
+  sm.addState("menu", new MenuState());
 
   document.body.appendChild(canvas);
 
   /* start the game */
 
-  sm.changeState("game");
+  sm.changeState("menu");
 
   console.log("bootstrapping loaded");
   loaded--;
