3537cb0aa0a78ae753309dfa3266831fc792f662
iver56 <iver56@hotmail.com>
2013-11-01 22:08:31 +0100

    One can now click/touch anywhere on the credits screen to get back to the menu. The back button on the achievements is now also touchable. Removed a few unnecessary lines in CreditsState.

--- src/CreditsState.js
+++ src/CreditsState.js
@@ -10,17 +10,19 @@ CreditsState.prototype.pause = function() {
 
 CreditsState.prototype.resume = function() {
   this.cooldown = true;
+  this.elements = [
+    [function() {
+      sm.changeState('menu');
+    }, {x: 0, y: 0, w: 16, h: 9}]
+  ];
 };
 
 CreditsState.prototype.render = function(ctx) {
   ctx.save();
   var scaler = 16 * GU / this.bg_img.width;
-  ctx.translate(16 * GU / 2, 9 * GU / 2);
   ctx.scale(scaler, scaler);
-  ctx.translate(-this.bg_img.width / 2, -this.bg_img.height / 2);
   ctx.drawImage(this.bg_img, 0, 0);
   ctx.restore();
-  ctx.save();
 };
 
 CreditsState.prototype.update = function() {
